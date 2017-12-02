e323caf4795d2571aef8aa294d535dff986b9827
persue123 <1220438725@qq.com>
2017-12-02 10:33:06 +0800

    123

--- entrys/insurance/modules/TopNav.jsx
+++ entrys/insurance/modules/TopNav.jsx
@@ -169,7 +169,6 @@ var TopNav = React.createClass({
                                     <i>文学生活</i>
                                 </Link>
                             </li>
-
                             <li className="nav_menu-item">
                                 <Link to={window.App.getAppRoute() + "/collection"}>
                                     <i>文学典藏</i>
